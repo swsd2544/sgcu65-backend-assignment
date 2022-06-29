4674b61307ae572297c70b5891f8af070562e971
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-06-29 21:59:56 +0700

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { PrismaService } from './prisma/prisma.service'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
 import config from './config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.use(helmet())
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
   await app.listen(config.port)
