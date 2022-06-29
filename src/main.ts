e2db25c4862d880dcb04df22a52ccd5507b1ed98
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-06-29 22:05:29 +0700

    add cookie-parser and csurf

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { PrismaService } from './prisma/prisma.service'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
+import csurf from 'csurf'
 import config from './config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   app.use(helmet())
+  app.use(cookieParser())
+  app.use(csurf())
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
   await app.listen(config.port)
