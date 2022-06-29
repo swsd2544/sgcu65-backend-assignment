eee1191a0fe25fd979a12b901ef8ab54e64aee5c
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-06-29 15:29:32 +0700

    Generate basic teams routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
 import { TasksModule } from './tasks/tasks.module'
+import { TeamsModule } from './teams/teams.module'
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, TasksModule],
+  imports: [PrismaModule, UsersModule, AuthModule, TasksModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
